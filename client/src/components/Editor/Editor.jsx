fb2d109a399c4c8a268950cabef1bab230885ce9
lana <lanabkim@gmail.com>
2021-03-22 16:27:20 -0700

    posting working/ not delete

--- client/src/components/Editor/Editor.jsx
+++ client/src/components/Editor/Editor.jsx
@@ -3,13 +3,13 @@ import CardAddForm from "../CardAddForm/CardAddForm";
 import CardEditForm from "../CardEditForm/CardEditForm";
 import style from "./Editor.module.css";
 
-const Editor = ({ cards, addCard, deleteCard, updateCard }) => (
+const Editor = ({ cards, onAdd, deleteCard }) => (
 	<section className={style.editor}>
 		<h3 className={style.title}>worry card add and edit</h3>
 		{Object.keys(cards).map((key) => (
-			<CardEditForm key={key} card={cards[key]} />
+			<CardEditForm key={key} card={cards[key]} deleteCard={deleteCard} />
 		))}
-		<CardAddForm onAdd={addCard} />
+		<CardAddForm onAdd={onAdd} />
 	</section>
 );
 
