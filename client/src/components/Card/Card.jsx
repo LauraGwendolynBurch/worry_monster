78e049021943ad630060f893027fb80df1a85ce5
lana <lanabkim@gmail.com>
2021-03-23 04:42:07 -0700

    styling for preview cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import Button from "../Button/Button";
 import styles from "./Card.module.css";
 
-const Card = ({ card }) => {
+const Card = ({ card, deleteCard, onEdit }) => {
 	const { city, body } = card;
+	const onSubmit = (event) => {
+		deleteCard(card);
+	};
 	return (
 		<li className={styles.card}>
 			<div className={styles.info}>
 				<h3 className={styles.city}>{city}</h3>
 				<p className={styles.body}>{body}</p>
+				<div className={styles.buttons}>
+					<button className={styles.button}>
+						Change Share Status to private to share
+					</button>
+					&nbsp;
+					<button onClick={onSubmit} className={styles.button}>
+						Delete and Feed it to Monster
+					</button>
+				</div>
 			</div>
 		</li>
 	);
--- client/src/components/Preview/Preview.jsx
+++ client/src/components/Preview/Preview.jsx
@@ -7,18 +7,19 @@ const Preview = ({ cards, deleteCard, onEdit }) => (
 	<section className={styles.preview}>
 		<h3 className={styles.title}> Card Preview</h3>
 		<ul className={styles.cards}>
-			{/* {Object.keys(cards).map((key) => (
-					<Card key={key} card={cards[key]} />
-				))} */}
-
 			{Object.keys(cards).map((key) => (
+					<Card key={key} card={cards[key]} deleteCard={deleteCard}
+					onEdit={onEdit} />
+				))}
+
+			{/* {Object.keys(cards).map((key) => (
 				<CardEditForm
 					key={key}
 					card={cards[key]}
 					deleteCard={deleteCard}
 					onEdit={onEdit}
 				/>
-			))}
+			))} */}
 		</ul>
 	</section>
 );
