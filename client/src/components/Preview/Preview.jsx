be870696b134c6cb57831e4a9078b8d5c97ad0db
lana <lanabkim@gmail.com>
2021-03-22 23:24:14 -0700

    removing cards from lefe side causing error

--- client/src/components/Preview/Preview.jsx
+++ client/src/components/Preview/Preview.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import styles from "./Preview.module.css";
 import Card from "../Card/Card";
+import CardEditForm from "../CardEditForm/CardEditForm";
 
-const Preview = ({ cards }) => (
-	
-		<section className={styles.preview}>
-			<h3 className={styles.title}> Card Preview</h3>
-			<ul className={styles.cards}>
-				{Object.keys(cards).map((key) => (
+const Preview = ({ cards, deleteCard, onEdit }) => (
+	<section className={styles.preview}>
+		<h3 className={styles.title}> Card Preview</h3>
+		<ul className={styles.cards}>
+			{/* {Object.keys(cards).map((key) => (
 					<Card key={key} card={cards[key]} />
-				))}
-			</ul>
-		</section>
-	
-)
+				))} */}
+
+			{Object.keys(cards).map((key) => (
+				<CardEditForm
+					key={key}
+					card={cards[key]}
+					deleteCard={deleteCard}
+					onEdit={onEdit}
+				/>
+			))}
+		</ul>
+	</section>
+);
 export default Preview;
