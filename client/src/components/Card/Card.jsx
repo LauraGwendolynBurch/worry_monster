a655f540114bb7a35404d696f8582520ec26f52b
lana <lanabkim@gmail.com>
2021-03-24 19:42:23 -0700

    commit to pull

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -8,21 +8,23 @@ const Card = ({ card, deleteCard, onEdit }) => {
 		deleteCard(card);
 	};
 	return (
-		<li className={styles.card}>
-			<div className={styles.info}>
-				<h3 className={styles.city}>{city}</h3>
-				<p className={styles.body}>{body}</p>
-				<div className={styles.buttons}>
-					<button className={styles.button}>
-						Change Share Status to private to share
-					</button>
-					&nbsp;
-					<button onClick={onSubmit} className={styles.button}>
-						Delete and Feed it to Monster
-					</button>
+		<form>
+			<li className={styles.card}>
+				<div className={styles.info}>
+					<h3 className={styles.city}>{city}</h3>
+					<p className={styles.body}>{body}</p>
+					<div className={styles.buttons}>
+						<button className={styles.button}>
+							Change Share Status to private to share
+						</button>
+						&nbsp;
+						<button onClick={onSubmit} className={styles.button}>
+							Delete and Feed it to Monster
+						</button>
+					</div>
 				</div>
-			</div>
-		</li>
+			</li>
+		</form>
 	);
 };
 
