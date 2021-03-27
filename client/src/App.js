9f4cb73d4bbb8a75a3bb25d219044ba3a1b234bc
Laura Gwen <lauraburch.lb@gmail.com>
2021-03-27 14:43:39 -0400

    changed app.js to go to main

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "./App.module.css";
 import Main from "./pages/Main";
 import Monster from "./pages/Monster";
- import SharedWorry from "./pages/SharedWorry";
+import SharedWorry from "./pages/SharedWorry";
 import Worry from "./pages/Worry";
 import ModalContainer from './components/ModalContainer'
 import HeaderContainer from "./components/HeaderContainer";
@@ -29,6 +29,7 @@ function App() {
 					<div>
 						{!isAuthenticated && <Main setLoginModal={setLoginModal} />}
 						{isAuthenticated && <HeaderContainer />}
+					<Route exact path="/" component={Main} />
 					<Route exact path="/Worry" component={isAuthenticated && Worry} />
 					<Route exact path="/SharedWorry" component={isAuthenticated && SharedWorry} />
 					<Route exact path="/Monster" component={isAuthenticated && Monster} />
